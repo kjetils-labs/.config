5122c3fd22c57fa856b3a2f413f6d6facc367922
kjetil <kjetil.sigvartsen@nhn.no>
2025-03-03 08:48:14 +0100

    Updated

--- kickstart.sh
+++ kickstart.sh
@@ -8,6 +8,13 @@ if ! grep -q 'source $HOME/.config/aliases' "$HOME/$fileName"; then
 	echo -e '\nsource $HOME/.config/aliases' >> $HOME/$fileName
 fi
 
+# ZSH sytax highlighting
+echo "Make sure 'zsh-syntax-highlighting' is installed"
+
+if ! grep -q 'ls ~/.oh-my-zsh/custom/plugins' "zsh-autosuggestions"; then 
+    echo "Installing zsh-autosuggestions"
+    echo "git clone https://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions"
+fi
 
 echo "sourcing"
 zsh
