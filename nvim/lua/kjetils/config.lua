cc47e84205b310711412e22ccd673be7a981bb68
kjetil <kjetil.sigvartsen@nhn.no>
2025-04-22 11:01:10 +0200

    added hover for error info, moved lsp-config info into per lsp language

--- nvim/lua/kjetils/config.lua
+++ nvim/lua/kjetils/config.lua
@@ -24,7 +24,28 @@ vim.opt.clipboard = {
     },
     paste = {
         ['+'] = 'xsel --clipboard --output',
-        ['+'] = 'xsel --primary --output',
+        ['*'] = 'xsel --primary --output',
     },
     cache_enabled = 0,
 }
+
+-- vim.diagnostic.config({
+--     underline = true,
+--     virtual_text = true,
+-- })
+
+vim.o.updatetime = 250
+vim.api.nvim_create_autocmd("CursorHold", {
+    buffer = bufnr,
+    callback = function()
+        local opts = {
+            focusable = false,
+            close_events = { "BufLeave", "CursorMoved", "InsertEnter", "FocusLost" },
+            border = 'rounded',
+            source = 'always',
+            prefix = ' ',
+            scope = 'cursor',
+        }
+        vim.diagnostic.open_float(nil, opts)
+    end
+})
