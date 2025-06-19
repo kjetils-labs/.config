75620e9f88b138ed928da540cfb441917e4a5346
kjetil <kjetil.sigvartsen@nhn.no>
2025-06-19 08:33:04 +0200

    linehighlighting default on

--- nvim/lua/plugins/gitsigns.lua
+++ nvim/lua/plugins/gitsigns.lua
@@ -23,6 +23,7 @@ return {
             current_line_blame_formatter = '<author>, <author_time:%R> - <summary>',
             sign_priority                = 6,
             max_file_length              = 40000, -- Disable if file is longer than this (in lines)
+            linehl                       = true,  -- Enables :Gitsigns toggle_numhl on launch
         })
-    end
+    end,
 }
