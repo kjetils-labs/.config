0342efe622070dbbf36b5daca089d3587d2c8266
Kjetil Sigvartsen <kjetil.sigvartsen@nhn.no>
2026-05-04 13:30:49 +0200

    changed filename to show full path or home path if applicable

--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -1,11 +1,19 @@
 return {
-    'nvim-lualine/lualine.nvim',
-    dependencies = { 'nvim-tree/nvim-web-devicons' },
-    config = function()
-        require("lualine").setup({
-            options = {
-                theme = "codedark"
-            }
-        })
-    end
+	"nvim-lualine/lualine.nvim",
+	dependencies = { "nvim-tree/nvim-web-devicons" },
+	config = function()
+		require("lualine").setup({
+			options = {
+				theme = "codedark",
+			},
+			sections = {
+				lualine_a = {
+					{
+						"filename",
+						path = 3,
+					},
+				},
+			},
+		})
+	end,
 }
